1ceeddf4bc0471d50bbc99031ce1161ad2f17817
371ChilakS <146194459+371ChilakS@users.noreply.github.com>
2023-09-28 20:46:34 +0100

    completed task

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,15 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for value in items:
+        value = str(value)
+        if value in frequencies.keys():
+            frequencies[value] += 1
+        else:
+            frequencies[value] = 1
+
     return frequencies
+# print(frequencies([1,"ten","1",1,1]))
+# print(frequencies([1,"ten","1",1,1,1.0]))
+# print(frequencies(['a', 'a', 'b', 'b', 'b', 'c']))
+# print(frequencies([100, 'Hello', '100', '100', 100]))
